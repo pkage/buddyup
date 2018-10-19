543144c65a0e1bfece4caf2b169062312e326296
andrius <agirdzius@gmail.com>
2018-10-20 01:28:16 +0200

    basic methods added

--- backend/quickstart.py
+++ backend/quickstart.py
@@ -6,31 +6,41 @@ from oauth2client import file, client, tools
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+now = datetime.datetime.utcnow()
 
+# gets upcoming events to a specified datetime
+def getUpcoming(service, calendar_id, year_end, month_end, day_end):
+    end = now.replace(year=year_end, month=month_end, day=day_end)
+    print('Upcoming events:')
+    events_result = service.events().list(calendarId=calendar_id, timeMin=now.isoformat()+'Z',
+                                        singleEvents=True, timeMax=end.isoformat() + 'Z',
+                                        orderBy='startTime').execute()
+    print(events_result)
+    return events_result
+
+# same as above but only gets free/busy status
+def getBusy(service, calendar_id, year_end, month_end, day_end):
+    end = now.replace(year=year_end, month=month_end, day=day_end)
+    print("Busy intervals:")
+    body = {
+        "timeMin": now.isoformat() + 'Z', # 'Z' indicates UTC time,
+        "timeMax": end.isoformat() + 'Z',
+        "items": [{'id':calendar_id}]
+    }
+    busy_result = service.freebusy().query(body=body).execute()
+    print(busy_result)
+    return busy_result
+
+# authenticates the user and gets service started
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
         flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('calendar', 'v3', http=creds.authorize(Http()))
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    getUpcoming(service, 'primary', 2018,10,31)
+    getBusy(service, 'primary', 2018,10,31)
 
 if __name__ == '__main__':
     main()
