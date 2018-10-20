dc28d2e08951d95be04cfb16e4c3525c9db4e4c2
Patrick Kage <quadnix1@gmail.com>
2018-10-21 01:50:02 +0200

    added auth cacher

---
+++ backend/authcache.py
@@ -0,0 +1,14 @@
+import json
+
+def save_auth(token):
+    with open('./tokencache.json', 'w') as fp:
+        json.write({
+            'token': token    
+        }, fp)
+
+def load_auth():
+    if not os.path.exists('./tokencache.json'):
+        raise Exception('token cache file not found')
+
+    with open('./tokencache.json', 'r') as fp:
+        return json.load(fp)['token']
